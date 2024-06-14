39dd998abca09e29166c055421ee6dbf9f969895
maximmuss699 <maxsamumax@gmail.com>
2024-06-14 14:29:57 +0200

    added OOP

--- main.py
+++ main.py
@@ -85,7 +85,6 @@ class ActionRecognition:
             cap.release()
             cv2.destroyAllWindows()
 
-
 # Запуск функции для обработки видео с веб-камеры
 if __name__ == "__main__":
     action_recognition = ActionRecognition('yolov8n.pt')
